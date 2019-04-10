8520fd2bcdb3ea8ac75c6b666f7732deb6b4b254
Sheeptune <gwenlhd@gmail.com>
2019-04-10 15:46:52 +0200

    Using promises

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
+// Promises avoid getting overlapping system like with callback
+// We use them to get a rest system (what happens next)
+
 console.log('Start')
 
-// Creation of a promise
-new Promise((resolve, reject)=> {
+// Get member is now a promise so we can use a .then to represents the 'resolve'
+getMember()
+    .then(member => getArticles(member))
+    // We get the new Promise of getArticles()
+    // So .then = the 'resolve' of 'getArtciles(member)' ABOVE
+    .then(articles => console.log(articles))
+    // catch will manage all the promises
+    . catch(err => console.log(err.message))
 
-    setTimeout(()=>{
-        resolve('All good')
-        //reject(new Error('Error during ...'))
-    }, 1500)
-})
-
-// We need to create resolve() and reject() with .then()
-// First resolve, then reject
-.then(message => {console.log(message)})
-.catch(err => {console.log(err.message)})
-
-console.log('End')
-
-
-// By convention we use 'next' when use callbacks
 // getMember() will be executed once the anonymous function (callback) is executed
-function getMember(next) {
-    setTimeout(()=>{
-        next('Member 1')
-    }, 1500)
+function getMember() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Member 1')
+            resolve('Member 1')
+        }, 1500)
+    })
 }
 
 // Function getArticles takes 'member' into account
 // It simulates a timeout and return an array with the function 'next'
 // We call this function once getMember() is executed
 
-function getArticles(member, next) {
-    setTimeout(()=>{
-        next([1, 2, 3])
-    }, 1500)
+function getArticles(member) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([1, 2, 3])
+        }, 1500)
+    })
 }
 
-// Lees promesses évite le systeme d'embrication pour avoir plutot un systeme de suite
+console.log('End')
+
+
+
