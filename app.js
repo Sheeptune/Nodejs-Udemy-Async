83b478f14466bca866085448214f6357c861bee4
Sheeptune <gwenlhd@gmail.com>
2019-04-10 11:23:21 +0200

    Starting with callbacks

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 console.log('Start')
 
 // Here we are testing async -> 'Timeout' is rendered after 'End'
+// USING CALLBACKS to display 'Member 1'
+getMember((member) => {
+    console.log(member)
+})
 
-getMember()
 console.log('End')
 
-function getMember(){
+// By convention we use 'next' when use callbacks
+// 'Member 1' is rendered once the setTimeout() ended
+function getMember(next) {
     setTimeout(()=>{
-        console.log('Member 1')
+        next('Member 1')
     }, 1500)
 }
+
+
 
