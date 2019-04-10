551fb27d469f5cfa733046504259d564086d049a
Sheeptune <gwenlhd@gmail.com>
2019-04-10 16:25:57 +0200

    Using Asyn and Await

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
-// Here we are gonna make promises in parallel
-
 console.log('Start')
 
-let p1 = new Promise((resolve, reject) => {
-    setTimeout(()=> {
-        resolve('p1')
-    }, 1500)
-})
+/*
+getMember()
+    .then(member => getArticles(member))
+    .then(articles => console.log(articles))
+}) */
 
-let p2 = new Promise((resolve, reject) => {
-    setTimeout(()=> {
-        resolve('p2')
-    }, 1500)
-})
+// Call getMembers(), wait for resolve to return a result, then put it back into the member
+// As if our code was synchroneous, simultaneous (but in reality it is not)
+async function viewArticles(){
+    // Await & Asyn require variables
+    // Await is looking for the result returned by resolve, here [1, 2, 3]
+    let member = await getMember()
+    let articles = await getArticles(member)
+    console.log(articles)
+}
 
-// We make a command to get both promises in parallel with all()
-// and an array containing the promises results
-// .all() is wainting for both reponses
-// .race() get the faster one
-Promise.all([p1, p2])
-    .then(result => console.log(result))
+viewArticles()
+
+function getMember() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Member 1')
+        }, 1500)
+    })
+}
+
+// Function getArticles takes 'member' into account
+// It simulates a timeout and return an array with the function 'next'
+
+function getArticles(member) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve([1, 2, 3])
+        }, 1500)
+    })
+}
+
 
 console.log('End')
-
-
-
