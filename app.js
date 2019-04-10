90dabd4fa026dd566fa90446b0d98ef1c5484195
Sheeptune <gwenlhd@gmail.com>
2019-04-10 13:57:27 +0200

    Managing several callback function simultaneously

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 console.log('Start')
 
-// Here we are testing async -> 'Timeout' is rendered after 'End'
-// USING CALLBACKS to display 'Member 1'
+// (After 'end') We want to return the member first and then the array
+
+// USING A FIRST CALLBACK on getMember() to display 'Member 1' 
 getMember((member) => {
     console.log(member)
+    // USING A SECOND CALLBACK on getArticles() to display the array
+    // We set articles as a callback parameter 
+    getArticles(member, (articles)=>{
+        console.log(articles)
+    })
 })
 
 console.log('End')
 
+
 // By convention we use 'next' when use callbacks
-// 'Member 1' is rendered once the setTimeout() ended
+// getMember() will be executed once the anonymous function (callback) is executed
 function getMember(next) {
     setTimeout(()=>{
         next('Member 1')
     }, 1500)
 }
 
+// Function getArticles takes 'member' into account
+// It simulates a timeout and return an array with the function 'next'
+// We call this function once getMember() is executed
 
-
+function getArticles(member, next) {
+    setTimeout(()=>{
+        next([1, 2, 3])
+    }, 1500)
+}
