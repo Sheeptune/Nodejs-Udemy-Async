8559d9a019065b8956b9ac160cdc2548a2d55402
Sheeptune <gwenlhd@gmail.com>
2019-04-10 16:02:51 +0200

    Using promises in parallel

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-// Promises avoid getting overlapping system like with callback
-// We use them to get a rest system (what happens next)
+// Here we are gonna make promises in parallel
 
 console.log('Start')
 
-// Get member is now a promise so we can use a .then to represents the 'resolve'
-getMember()
-    .then(member => getArticles(member))
-    // We get the new Promise of getArticles()
-    // So .then = the 'resolve' of 'getArtciles(member)' ABOVE
-    .then(articles => console.log(articles))
-    // catch will manage all the promises
-    . catch(err => console.log(err.message))
+let p1 = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        resolve('p1')
+    }, 1500)
+})
 
-// getMember() will be executed once the anonymous function (callback) is executed
-function getMember() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('Member 1')
-            resolve('Member 1')
-        }, 1500)
-    })
-}
+let p2 = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        resolve('p2')
+    }, 1500)
+})
 
-// Function getArticles takes 'member' into account
-// It simulates a timeout and return an array with the function 'next'
-// We call this function once getMember() is executed
-
-function getArticles(member) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve([1, 2, 3])
-        }, 1500)
-    })
-}
+// We make a command to get both promises in parallel with all()
+// and an array containing the promises results
+// .all() is wainting for both reponses
+// .race() get the faster one
+Promise.all([p1, p2])
+    .then(result => console.log(result))
 
 console.log('End')
 
